f0737dbe84c3beca747cee82333d0eca7b7a01ff
yuyakato2022 <102748219+yuyakato2022@users.noreply.github.com>
2022-04-30 05:43:04 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "homes#top"
+  devise_for :users
+  get "home/about"=>"homes#about"
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:create,:update]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
